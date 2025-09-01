0a4829f9037bd16eed1e8fbebf551ecf29a72310
jaynopponep <noppone.p@gmail.com>
2025-09-01 15:59:19 -0400

    added notes for HW1a.cpp: initializeGL & resizeGL()

--- skeleton/hw1/HW1a.cpp
+++ skeleton/hw1/HW1a.cpp
@@ -8,6 +8,9 @@
 // ===============================================================
 
 #include "HW1a.h"
+// ** NOTE
+// version we're running is 5.14.2, latest will not compile correctly!
+// **
 
 // init array of 16 vertices for letter 'P'
 float Vertices[] = {
@@ -52,7 +55,6 @@ HW1a::HW1a(const QGLFormat &glf, QWidget *parent)
 }
 
 
-
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // HW1a::initializeGL:
 //
@@ -62,7 +64,16 @@ HW1a::HW1a(const QGLFormat &glf, QWidget *parent)
 void
 HW1a::initializeGL()
 {
-	// PUT YOUR CODE HERE
+	// https://doc.qt.io/archives/qt-5.15/qglfunctions.html#initializeGLFunctions
+	// part of QGLFunctions class (Qt 4.8), now obsolete/deprecated in latest Qt version
+	initializeGLFunctions(); // <- this just binds the available GL functions to Qt for us to use
+
+	// the GL functions loaded can be found in: https://docs.gl/gl4/glClearColor
+	// black background
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	
+	// we need 3x3 letter P shapes bottom to top, left to right
+	// we'll use glColor3f in paintGL() since it's situational
 }
 
 
@@ -76,7 +87,30 @@ HW1a::initializeGL()
 void
 HW1a::resizeGL(int w, int h)
 {
-	// PUT YOUR CODE HERE
+	// ar = aspect ratio
+	float xmax, ymax;
+	float ar = (float) w / h;
+	if (ar > 1.0) { // wide screen
+		xmax = ar;
+		ymax = 1.0;
+	} else { // tall screen
+		xmax = 1.0;
+		ymax = 1.0 / ar;
+	}
+
+	// set viewport to occupy full canvas
+	glViewport(0, 0, w, h); // 0, 0 = x, y; location of lower left corner of viewport
+
+	// init viewing coordinates for orthographic projection
+	glMatrixMode(GL_PROJECTION); // matrix ops to the projection matrix stack
+	// (explained in Chapter 3.8 in textbook: Projection Matrices)
+	glLoadIdentity(); // init curr matrix = Identity Matrix
+
+	glOrtho(-xmax, xmax, -ymax, ymax, -1.0, 1.0); // curr matrix *= ortho matrix
+	// orthographic projection -> is to map 3D to 2D to make the projection matrix
+	// view work. parameters in glOrtho() are corners of the coord system
+	
+	// (btw, pretty much just took the same setting from HW0a.cpp)
 }
 
 
